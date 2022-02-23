a1f513fbbc152742ceb7c5d47925b44b49070c7c
aaaa <aaaa>
2022-02-24 07:47:53 +0900

    Initial Commit

---
+++ FadeInAlarm_v2_/FadeInAlarm_v2_App.swift
@@ -0,0 +1,17 @@
+//
+//  FadeInAlarm_v2_App.swift
+//  FadeInAlarm_v2_
+//
+//  Created by nnname on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FadeInAlarm_v2_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
