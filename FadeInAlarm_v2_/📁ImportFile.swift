70170ffd481faf252550d91a604903c7fc53f948
aaaa <aaaa>
2022-05-08 21:29:04 +0900

    a

--- FadeInAlarm_v2_/📁ImportFile.swift
+++ FadeInAlarm_v2_/📁ImportFile.swift
@@ -24,6 +24,31 @@ struct 📁ImportFile: View {
             📁FilePreview()
         }
         .padding()
+        .fileImporter(isPresented: $📱.📂ImporterAppear, allowedContentTypes: [.audio]) { 🅁esult in
+            let 🗄 = FileManager.default
+            let 🗃 = 🗄.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            
+            do {
+                let 📦 = try 🅁esult.get()
+                
+                do {
+                    let 🗂 = try 🗄.contentsOfDirectory(at: 🗃, includingPropertiesForKeys: nil)
+                    if let 📍 = 🗂.first {
+                        do { try 🗄.removeItem(at: 📍) } catch { print("👿", error) }
+                    }
+                } catch { print(error) }
+                
+                let 🄽ewURL = 🗃.appendingPathComponent(📦.lastPathComponent)
+                
+                if 📦.startAccessingSecurityScopedResource() {
+                    do {
+                        try 🗄.copyItem(at: 📦, to: 🄽ewURL)
+                        📱.💽Name = 🄽ewURL.lastPathComponent
+                    } catch { print("👿", error) }
+                }
+                📦.stopAccessingSecurityScopedResource()
+            } catch { print("👿", error) }
+        }
     }
 }
 
